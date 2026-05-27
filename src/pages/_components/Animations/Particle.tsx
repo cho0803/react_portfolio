4d4071dce4928b4cd740744096e1d2baf7fb6d48
cho0803 <cho0807s@naver.com>
2026-05-27 11:16:44 +0900

    Particle 컴포넌트 랜더링시  한번만 실행하도록  수정

--- src/pages/_components/Animations/Particle.tsx
+++ src/pages/_components/Animations/Particle.tsx
@@ -6,14 +6,22 @@ import React from 'react';
 interface ParticleProps {
   particle?: number;
 }
+interface ParticleItem {
+  id: number;
+  left: string;
+  top: string;
+  delay: string;
+}
 
 export default ({ particle = 300 }: ParticleProps) => {
-    const particles = [...Array(particle)].map((_, i) => ({
+    const particles = useMemo<ParticleItem[]>(() => {
+    return [...Array(particle)].map((_, i) => ({
       id: i,
       left: `${Math.random() * 100}%`,
       top: `${Math.random() * 100}%`,
       delay: `${i * 0.05}s`
     }));
+  }, [particle]);
   return (
     <div className={styles.branding}> 
       {particles.map((p) => (
