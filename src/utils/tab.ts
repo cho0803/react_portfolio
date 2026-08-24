89f2c213ad71f8ead5b630fe041e11454ebe2358
cho0803 <cho0807s@naver.com>
2026-08-24 15:51:12 +0900

    메뉴 리스트 추가

---
+++ src/utils/tab.ts
@@ -0,0 +1,48 @@
+const TAB_ITEM = [
+  {
+    tab: 'REACT PROJECT',
+    key: 'react',
+    items: [
+      {
+        id: 1,
+        item: 'Portfolio',
+        key: 'react',
+      },
+      {
+        id: 2,
+        item: 'Resume',
+        key: 'react',
+      }, 
+      {
+        id: 3,  
+        item: 'Leaflet.js Map',
+        key: 'about',
+      }, 
+    ],
+  },
+  {
+    tab: 'ANIMATION',
+    key: 'animation',
+    items: [
+      {
+        id: 1,
+        item: 'Particle',
+      },
+      {
+        id: 2,
+        item: 'Spinning',
+      },
+    ],
+  },
+]
+
+const getContents = (value: string) => {
+  const NEW_LIST = TAB_ITEM.filter((item) => {
+    return item.key === value
+  })
+  return NEW_LIST[0].items
+}
+
+const baseUrl = 'projects'
+
+export { TAB_ITEM, baseUrl, getContents }
